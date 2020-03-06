4d9f6b0ca4510a4658ec662aaf1473795c5bfd75
jmacd <jmacd@lightstep.com>
2020-03-05 22:13:03 -0800

    4 color patterns

--- cmd/play/play.go
+++ cmd/play/play.go
@@ -15,31 +15,21 @@ const (
 	pixels = 300 // Lies (it's 299)
 	width  = 20
 	height = 15
+
+	epsilon = 0.00001
 )
 
-var keypoints = GradientTable{
-	{MustParseHex("#9e0142"), 0.0},
-	{MustParseHex("#d53e4f"), 0.1},
-	{MustParseHex("#f46d43"), 0.2},
-	{MustParseHex("#fdae61"), 0.3},
-	{MustParseHex("#fee090"), 0.4},
-	{MustParseHex("#ffffbf"), 0.5},
-	{MustParseHex("#e6f598"), 0.6},
-	{MustParseHex("#abdda4"), 0.7},
-	{MustParseHex("#66c2a5"), 0.8},
-	{MustParseHex("#3288bd"), 0.9},
-	{MustParseHex("#5e4fa2"), 1.0},
-}
+type (
+	Buffer [pixels]colorful.Color
 
-type Buffer [pixels]colorful.Color
+	Sender struct {
+		dest *net.UDPAddr
+		conn *net.UDPConn
 
-type Sender struct {
-	dest *net.UDPAddr
-	conn *net.UDPConn
-
-	Buffer
-	packet.ArtDMXPacket
-}
+		Buffer
+		packet.ArtDMXPacket
+	}
+)
 
 func newSender() *Sender {
 	dst := fmt.Sprintf("%s:%d", ipAddr, packet.ArtNetPort)
@@ -63,16 +53,57 @@ func main() {
 	sender := newSender()
 	buffer := sender.Buffer[:]
 
-	for {
-		for i := 0; i < 100; i++ {
-			c := keypoints.GetInterpolatedColorFor(float64(i) / float64(100))
+	start := time.Now()
 
-			for i := 0; i < pixels; i++ {
-				buffer[i] = c
+	for {
+		seconds := time.Now().Sub(start) / time.Second
+
+		pattern := (seconds / 15) % 4
+
+		for twoCount := 0.0; twoCount < 2; twoCount += 0.0005 {
+			level := twoCount
+			if level >= 1 {
+				level = 2 - level
+			}
+
+			for y := 0; y < height; y++ {
+				for x := 0; x < width; x++ {
+					var c colorful.Color
+
+					switch pattern {
+					case 0:
+						c = colorful.Lab(
+							level,
+							2*(float64(x)/(width-1)-0.5),
+							2*(float64(y)/(height-1)-0.5),
+						)
+					case 1:
+						c = colorful.Xyy(
+							(float64(y)+epsilon)/(height-1+epsilon),
+							(float64(x)+epsilon)/(width-1+epsilon),
+							level,
+						)
+					case 2:
+						c = colorful.Xyz(
+							float64(x)/(width-1),
+							float64(y)/(height-1),
+							level,
+						)
+					case 3:
+						c = colorful.Luv(
+							level,
+							2*(float64(x)/(width-1)-0.5),
+							2*(float64(y)/(height-1)-0.5),
+						)
+					}
+
+					buffer[y*width+x] = c.Clamped()
+
+				}
 			}
 
 			sender.send()
-			time.Sleep(time.Millisecond * 160)
+			time.Sleep(time.Millisecond * 10)
 		}
 	}
 }
@@ -90,12 +121,10 @@ func (s *Sender) send() {
 
 		for i := 0; i < num; i++ {
 			c := s.Buffer[p+i]
-			data[i*3+0] = byte(c.B * 255)
+			data[i*3+0] = byte(c.R * 255)
 			data[i*3+1] = byte(c.G * 255)
-			data[i*3+2] = byte(c.R * 255)
+			data[i*3+2] = byte(c.B * 255)
 		}
-
-		s.ArtDMXPacket.SubUni++
 
 		b, _ := s.ArtDMXPacket.MarshalBinary()
 
@@ -104,7 +133,7 @@ func (s *Sender) send() {
 			panic(fmt.Sprint("error writing packet: ", err))
 		}
 
-		//fmt.Println("Packet at P", p)
+		s.ArtDMXPacket.SubUni++
 		p += num
 	}
 }
@@ -127,12 +156,4 @@ func (self GradientTable) GetInterpolatedColorFor(t float64) colorful.Color {
 
 	// Nothing found? Means we're at (or past) the last gradient keypoint.
 	return self[len(self)-1].Col
-}
-
-func MustParseHex(s string) colorful.Color {
-	c, err := colorful.Hex(s)
-	if err != nil {
-		panic("MustParseHex: " + err.Error())
-	}
-	return c
 }
